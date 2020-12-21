c6a17d6b68ef93cfd657aa78095b7229fd51e59a
David Grimmett <davidsgrimmett@gmail.com>
2020-12-20 20:30:27 -0500

    Update config.py

--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "335521b35734fcc972cb824b513189f4"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCcr8Zvsu8JxQnfdH-24uaEGS6KD3lmoZQ"
+g_key = ""
